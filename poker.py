429b259e12a1ef448fd5de510d509b81155b65f6
Argha Sen <iamslowdeath2gmail.com>
2014-12-14 18:32:57 +0530

    Write the handRank function

--- poker.py
+++ poker.py
@@ -3,10 +3,28 @@
 # Start Date : 14th December 2014
 # Last Update: 14th December 2014
 
-def poker( hands ):
+def poker(hands):
 	"Given a set of hands find the best hand"
-	return max( hands, key = hand_rank );
+	return max(hands, key = handRank);
 
-def hand_rank( hand ):
+def handRank(hand ):
 	"Given a hand return the rank of the hand"
-	return 0;
+	ranks = cardRanks(hand);
+	if straight(ranks) and flush(hand):
+		return (8, max(ranks)) # Highest card in the stright gives the entire hand.
+	elif kind(4, ranks):
+	 	return (7, kind(4, ranks), kind(1, ranks)) # Break ties by using which card is 4 times and if still equal use the final card. 
+	elif fullHouse(hand):
+		return (6, kind(3, ranks), kind(2,ranks)) #Break ties by using the cards in the full house.
+	elif flush(hand):
+		return (5, ranks)
+	elif stright(hands)
+		return (4, max(ranks))
+	elif kind(3, ranks)
+		return (3, kind(3, ranks), ranks)
+	elif twoPair(hands):
+		return (2, twoPair(ranks), ranks)
+	elif kind(2, ranks)
+		return (1, kind(2, ranks), ranks)
+	else:
+		return (0, ranks);
