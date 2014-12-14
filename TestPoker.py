d1ef3b9680f6e5e1e5f6343ef07f6f0e7e013b5d
Argha Sen <iamslowdeath2gmail.com>
2014-12-14 17:34:57 +0530

    initial test file

---
+++ TestPoker.py
@@ -0,0 +1,14 @@
+# Test File for Poker
+# Author: Argha Sen
+# Start Date : 14th December 2014
+# Last Update: 14th December 2014
+import poker
+def testPoker():
+	"Test cases for the functions in poker program"
+	sf = "6C 7C 8C 9C TC".split() # Straight Flush
+	fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
+	fh = "TD TC TH 7C 7D".split() # Full House
+	assert poker.poker( [ sf, fk, fh ] ) == sf
+	return "tests pass"
+
+print testPoker()
