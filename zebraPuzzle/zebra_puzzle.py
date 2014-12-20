52b64bbcc51d94c04ec83d2ccc7cef1f7f91a809
Argha Sen <iamslowdeath2gmail.com>
2014-12-20 12:39:16 +0530

    Move ifs where can be evaluated early

--- zebraPuzzle/zebra_puzzle.py
+++ zebraPuzzle/zebra_puzzle.py
@@ -26,22 +26,22 @@ def zebra_puzzle():
     return next(
         (water, zebra)
         for (red, green, ivory, yellow, blue) in orderings
+        if imright(green, ivory)  # 6
         for (englishman, spaniard, ukranian, japanese, norwegian) in orderings
-        for (coffee, tea, milk, ojdrink, water) in orderings
-        for (oldgold, kools, chesterfields, luckystrike, parliaments) in orderings
-        for (dog, snails, fox, horse, zebra) in orderings
         if englishman is red  # 2
-        if spaniard is dog  # 3
+        if norwegian is first  # 10
+        if nextto(norwegian, blue)  # 15
+        for (coffee, tea, milk, ojdrink, water) in orderings
         if coffee is green  # 4
         if ukranian is tea  # 5
-        if imright(green, ivory)  # 6
-        if oldgold is snails  # 7
-        if kools is yellow  # 8
         if milk is middle  # 9
-        if norwegian is first  # 10
+        if luckystrike is ojdrink  # 13
+        for (oldgold, kools, chesterfields, luckystrike, parliaments) in orderings
+        if kools is yellow  # 8
+        if japanese is parliaments  # 14
+        for (dog, snails, fox, horse, zebra) in orderings
+        if spaniard is dog  # 3
+        if oldgold is snails  # 7
         if nextto(chesterfields, fox)  # 11
         if nextto(kools, horse)  # 12
-        if luckystrike is ojdrink  # 13
-        if japanese is parliaments  # 14
-        if nextto(norwegian, blue)  # 15
     )
