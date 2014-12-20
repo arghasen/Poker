e0cc588b1fa9b3ecd25103f712c9e588eb8e94df
Argha Sen <iamslowdeath@gmail.com>
2014-12-20 14:03:38 +0530

    improve utils

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@
 # Author: Argha Sen
 # Start Date : 20th December 2014
 import time
+import numpy
 
 
 def timed_call(func, *args):
@@ -11,7 +12,14 @@ def timed_call(func, *args):
     t1 = time.clock()
     return t1 - t0, result
 
-def timed_calls( n, func, *args):
-    " call func n times with args, and print stats"
-    times =[timed_call(func, *args)[0] for _ in range(n)]
-    return max(times), average(times), max(times)
+
+def timed_calls(n, func, *args):
+    " call func n times with args, and print stats \
+    else run for n seconds "
+    if isinstance(n, int):
+        times = [timed_call(func, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timed_call(func, *args)[0])
+    return max(times), numpy.mean(times), max(times)
