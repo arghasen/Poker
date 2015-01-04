644f0b967f72f20f799b6a84166818159dde37f8
Argha Sen <iamslowdeath@gmail.com>
2015-01-04 17:49:50 +0530

    Soduku project started

---
+++ Soduku/Board.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 4, 2015
+
+@author: argha
+'''
+
+class Board(object):
+    '''
+    This is the board of the soduku
+    '''
+
+
+    def __init__(self, board_string):
+        '''
+        Constructor
+        '''
+        
---
+++ Soduku/SodukuException.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 4, 2015
+
+@author: argha
+'''
+
+class SodukuException(Exception):
+    '''
+    Exceptions raised by Soduku
+    '''
+    pass
+        
