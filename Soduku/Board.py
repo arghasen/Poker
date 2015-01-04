ce9cf9a63a0196b2c03def0f0d08e43e89629ecc
Argha Sen <iamslowdeath@gmail.com>
2015-01-04 21:07:02 +0530

    Added row reduction

--- Soduku/Board.py
+++ Soduku/Board.py
@@ -10,11 +10,39 @@ class Board(object):
     '''
     This is the board of the soduku
     '''
+
+    
+    def reduce_keys_from_rows(self, x, y):
+        print x,y
+        for z in xrange(self.vertical_size):
+            if self.grid[x][z] != 0:
+                self.grid_val[x] = [a.replace(self.grid[x][z], '')  if len(a)>1 else a for a in self.grid_val[x]]
+                #pass
+    
+    def reduce_keys_from_colums(self, x, y):
+        pass
+    
+    
+    def reduce_keys_from_box(self, x, y):
+        pass
+    
+    
+    def reduce_candidate_keys(self):
+        for x in xrange(self.horizontal_size):
+            for y in xrange(self.vertical_size):
+                if self.grid[x][y] == 0:
+                    self.reduce_keys_from_rows(x, y)
+                    self.reduce_keys_from_colums(x, y)
+                    self.reduce_keys_from_box(x, y)
+        self.print_grid(True)
+         
+                
     def create_candidate_keys(self):
         self.grid_val = [[0 for _ in xrange(self.vertical_size) ] for _ in xrange(self.horizontal_size)]
         for x in xrange(self.horizontal_size):
             for y in xrange(self.vertical_size):
-                self.grid_val[x][y] = '123456789' if self.grid[x][y] == 0 else self.grid[x][y] 
+                self.grid_val[x][y] = '123456789' if self.grid[x][y] == 0 else str(self.grid[x][y]) 
+        self.reduce_candidate_keys()        
                 
     def board_grid(self):
         '''
@@ -25,6 +53,7 @@ class Board(object):
             for y in xrange(self.vertical_size):
                 val = self.board_string[x * self.vertical_size + y]
                 self.grid[x][y] = val if val.isdigit() else 0
+           
                 
     def print_grid(self, values=False):
         width = 1
@@ -41,8 +70,10 @@ class Board(object):
                 print '\n' + '-' * (9 * width + 20),
             print
     
+    
     def get_Grid(self):
         return self.grid
+        
         
     def __init__(self, board_string):
         '''
