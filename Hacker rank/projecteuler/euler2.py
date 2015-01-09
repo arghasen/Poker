fd055ce14897ab991e85640c085e71e0d073277a
Argha Sen <iamslowdeath@gmail.com>
2015-01-09 18:07:50 +0530

    solve problem2

--- Hacker rank/projecteuler/euler2.py
+++ Hacker rank/projecteuler/euler2.py
@@ -1,2 +1,15 @@
 count = int(raw_input())
 for i in range(count):
+    maxval = int(raw_input())
+    a=1
+    b=1
+    c=0
+    sum =0
+    while c<=maxval:
+        if c%2==0:
+            sum+=c
+        c = a+b
+        a= b
+        b= c
+
+    print sum
